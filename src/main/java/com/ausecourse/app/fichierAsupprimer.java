87e45e6d8ef7c2f7c72686ac723cb03113627682
Ouvry Ludovic <ludovic.ouvry.etu@univ-lille.fr>
2018-11-13 11:53:39 +0100

    merge

--- src/main/java/com/ausecourse/app/fichierAsupprimer.java
+++ src/main/java/com/ausecourse/app/fichierAsupprimer.java
@@ -1,8 +1,7 @@
-package com.ausecourse.app;
+package com.ausecourse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class DemoApplication {
--- src/main/java/com/ausecourse/dao/UserDaoImpl.java
+++ src/main/java/com/ausecourse/dao/UserDaoImpl.java
@@ -8,6 +8,12 @@ import com.ausecourse.model.User;
 import com.ausecourse.repository.UserRepository;
 
 public class UserDaoImpl implements IUserDao{
-
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public Optional<User> getById(String id) {
+		return userRepository.findById(id);
+	}
 
 }
