a5f5db157d37a03e319d1ac776ff4a4ecf23f68c
Ouvry Ludovic <ludovic.ouvry.etu@univ-lille.fr>
2018-11-13 11:05:35 +0100

    client don't work because controlleur don't found

--- src/main/java/com/ausecourse/controller/UserController.java
+++ src/main/java/com/ausecourse/controller/UserController.java
@@ -5,10 +5,12 @@ import java.util.Optional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.ausecourse.dao.IUserDao;
 import com.ausecourse.model.User;
 
+@RestController
 public class UserController {
 //routes
 	IUserDao uDao;
