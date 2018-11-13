cdc417df80c15c68e9665df95778fc4124eace94
Ouvry Ludovic <ludovic.ouvry.etu@univ-lille.fr>
2018-11-13 08:53:26 +0100

    User class

--- src/main/java/com/ausecourse/model/User.java
+++ src/main/java/com/ausecourse/model/User.java
@@ -1,5 +1,85 @@
 package com.ausecourse.model;
 
 public class User {
+	private int id;
+	private String name;
+	private String nickname;
+	private String email;
+	private int tel;
+	private int numeroRoad;
+	private String Road;
+	private String city;
+	private String country; // tout en maj
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getTel() {
+		return tel;
+	}
+
+	public void setTel(int tel) {
+		this.tel = tel;
+	}
+
+	public int getNumeroRoad() {
+		return numeroRoad;
+	}
+
+	public void setNumeroRoad(int numeroRoad) {
+		this.numeroRoad = numeroRoad;
+	}
+
+	public String getRoad() {
+		return Road;
+	}
+
+	public void setRoad(String road) {
+		Road = road;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
 }
